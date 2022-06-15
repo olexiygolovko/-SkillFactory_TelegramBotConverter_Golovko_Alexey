db7779bce49ee9469103c4f57d6c49c5a9775f67
Olexiy <98397972+olexiygolovko@users.noreply.github.com>
2022-06-15 21:02:31 +0300

    Golovko: made adjustments to the error handler

--- extensions.py
+++ extensions.py
@@ -14,20 +14,20 @@ class Convertor:
         try:
             base_key = keys[base.lower()]
         except KeyError:
-            raise APIException(f"Укажите валюту верно - {base}!")
+            raise APIException(f"Не верно указана валюта - {base}! 👉 /start")
 
         try:
             sym_key = keys[sym.lower()]
         except KeyError:
-            raise APIException(f"Укажите валюту верно - {sym}!")
+            raise APIException(f"Не верно указана валюта - {sym}! 👉 /start")
 
         if base_key == sym_key:
-            raise APIException(f'Невозможно конвертировать одинаковые валюты - {base}!')
+            raise APIException(f'Невозможно конвертировать одинаковые валюты - {base}! 👉 /start')
 
         try:
             amount = float(amount)
         except ValueError:
-            raise APIException(f'Укажите объем конвертируемой валюты верно - {amount}!')
+            raise APIException(f'Не верно указано количество валюты - {amount}! 👉 /start')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_key}&tsyms={sym_key}')
         total_base = json.loads(r.content)[keys[sym.lower()]] * amount
